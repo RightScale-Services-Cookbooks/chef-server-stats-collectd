5e38b43032ff3d99d26587fd18fc8a082527cdd7
Richard Shade <rshade@rightscale.com>
2014-07-01 17:05:30 -0500

    removing unecessary depends on rightscale

--- metadata.rb
+++ metadata.rb
@@ -7,11 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 depends "rightscale"
-depends "yum"
-depends "collectd"
 depends "chef-server"
-depends "ruby"
 depends "chef-server-blueprint"
-depends "rubygems_install"
 
 recipe "chef_server_stats_collectd::default", "installs chef-server-stats and collectd plugin"
